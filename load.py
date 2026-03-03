dc2778d9575b6b327275481bfc14c22266ede3a9
Arslan Razin <ar.razin@vkteam.ru>
2026-03-04 01:34:10 +0300

    Add files

---
+++ load.py
@@ -0,0 +1,15 @@
+import os
+from catboost import CatBoostClassifier
+
+def get_model_path(path: str) -> str:
+    if os.environ.get("IS_LMS") == "1":  # проверяем где выполняется код в лмс, или локально. Немного магии
+        MODEL_PATH = '/workdir/user_input/model'
+    else:
+        MODEL_PATH = path
+    return MODEL_PATH
+
+def load_models():
+    model = CatBoostClassifier()
+
+    model_path = get_model_path("/my/super/path")
+    return model.load_model(model_path)
